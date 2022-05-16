fc91292ee6675b4ed3bfaac199dc5028b5e3c284
hoangharry <hoang.ho.it99@gmail.com>
2022-05-16 15:58:33 +0700

    update format

--- utils.py
+++ utils.py
@@ -16,7 +16,7 @@ def load_wav_to_torch(full_path):
 
 
 def load_filepaths_and_text(filename, split="|"):
-    with open(filename, encoding='utf8') as f:
+    with open(filename, encoding='ISO-8859-1') as f:
         filepaths_and_text = [line.strip().split(split) for line in f]
     return filepaths_and_text
 
